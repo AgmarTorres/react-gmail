fea171bbdc4c484dc9e7ee849c53167a07a2ffe9
Agmar Torres <agmartorres@gmail.com>
2020-12-26 17:06:23 -0300

    list e-mail

---
+++ src/components/EmailRow/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles.css";
+
+import { Checkbox, IconButton } from "@material-ui/core";
+import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+
+function EmailRow({ id, title, subject, description, time }) {
+  return (
+    <div className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+      <h3 className="emailRow__title">{title} </h3>
+      <div className="emailRow__message">
+        <h4>
+          {subject}
+          <span className="emailRow__description"> - {description}</span>
+        </h4>
+      </div>
+      <div className="emailRow__description">{time}</div>
+    </div>
+  );
+}
+
+export default EmailRow;
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -10,6 +10,10 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+import { IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
@@ -32,6 +36,19 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} title="Sent" number="54" />
       <SidebarOption Icon={NoteIcon} title="Drafts" number="54" />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number="54" />
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
