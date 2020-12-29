659f196798d8e3bfd1b199fb768c231df8f63b63
Agmar Torres <agmartorres@gmail.com>
2020-12-29 18:40:31 -0300

    Added sendMail, listEmail, renderEmail

--- src/components/EmailList/index.jsx
+++ src/components/EmailList/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 import { Checkbox, IconButton } from "@material-ui/core";
@@ -14,8 +14,23 @@ import PeopleIcon from "@material-ui/icons/People";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import EmailRow from "../EmailRow";
 import Section from "../Section";
+import { db } from "../../domain/firebase";
 
 function Header() {
+  const [emails, setEmails] = useState([]);
+  useEffect(() => {
+    db.collection("emails")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setEmails(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
   return (
     <div className="emailList">
       <div className="emailList__settings">
@@ -53,19 +68,18 @@ function Header() {
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
         <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
       </div>
+
       <div className="emailList__list">
-        <EmailRow
-          title="Twitch"
-          subject="Hey fellow streamer!!!"
-          description="This is a test"
-          time="10pm"
-        />
-        <EmailRow
-          title="Twitch"
-          subject="Hey fellow streamer!!!"
-          description="This is a test"
-          time="10pm"
-        />
+        {emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+          <EmailRow
+            id={id}
+            key={id}
+            title={to}
+            subject={subject}
+            description={message}
+            time={new Date(timestamp?.seconds * 1000).toUTCString()}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/EmailRow/index.jsx
+++ src/components/EmailRow/index.jsx
@@ -5,13 +5,29 @@ import { Checkbox, IconButton } from "@material-ui/core";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectMail } from "../../features/mailSlice";
 
 function EmailRow({ id, title, subject, description, time }) {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
+    );
+    history.push("/mail");
+  };
 
   return (
     <div
       onClick={() => {
+        openMail();
         history.push("/mail");
       }}
       className="emailRow"
