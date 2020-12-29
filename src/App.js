acaca2130f503bf265e42caa733f2d8e892f625e
Agmar Torres <agmartorres@gmail.com>
2020-12-29 20:53:47 -0300

    login and logou with firebase

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Mail from "./components/Mail";
 import EmailList from "./components/EmailList";
 import SendEmail from "./components/SendEmail";
+import Login from "./components/Login";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
+import { login, selectUser } from "./features/userSlice";
+import { auth } from "./domain/firebase";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
-  console.log(sendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        //user logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      } else {
+      }
+    });
+  }, []);
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendEmail />}
         </div>
-        {sendMessageIsOpen && <SendEmail />}
-      </div>
+      )}
     </Router>
   );
 }
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,41 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import { auth, provider } from "../../domain/firebase";
+import "./styles.css";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
+
+function Login() {
+  const dispatch = useDispatch();
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg"
+          alt="logo"
+        />
+        <Button variant="contained" color="primary" onClick={signIn}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
